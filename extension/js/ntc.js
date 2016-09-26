3532b2246e33b07b4e9c148c959114a01f140c06
mirova <tree.andrei@gmail.com>
2016-09-25 22:17:47 -0400

    Added time-remaining detection

--- extension/js/ntc.js
+++ extension/js/ntc.js
@@ -16,15 +16,25 @@ setInterval(function() {
 function ntc_main() {
   if (pageIsWatch()) {
     cacheSession();
+    startUpdateTimer();
   }
   else {
-    clearSession();
+    reset_ntc();
   }
 
 }
 
+
+function startUpdateTimer() {
+  // We keep the interval as faster than 1s to minimize comm delays. This helps alleviate problem if the Netflix timer starts before our timer does, and as a result our timer always has a <1s delay. This might be overkill though.
+  window.ntc.updateTimer = setInterval(updateCurrentVideoTime, 100);
+}
+
 // Clears ntc structure. This may be overkill.
-function clearSession() {
+function reset_ntc() {
+
+  clearInterval(window.ntc.updateTimer);
+  
   window.ntc = {};
 }
 
@@ -37,22 +47,25 @@ function pageIsWatch() {
 }
 
 
+// Checks the DOM for the current time in the video. If we find one, store it on the ntc structure.
+function updateCurrentVideoTime() {
+  var sliderList = document.getElementsByClassName('player-slider');
+
+  if (sliderList.length)
+    window.ntc.video.time = sliderList[0].children[0].innerHTML;  // e.g. "44:58"
+}
+
+
 // Caches video and userdata
 function cacheSession() {
-  
-  var userInfo,
-      videoId;
 
   // User data (we'll take everything for now, but only use what we need on the back-end)
   window.ntc.user = window.netflix.reactContext.models.userInfo.data;
 
-
   // Video data
-  videoId = window.location.pathname.substr(7);    // Offset is for the '/watch/' part of the URL
-  // videoType = window.netflix.falkorCache.videos[videoId].summary.type;
-
-  // Currently we don't differentiate what we take - movies or episodes. We just take the ID. This might change in the future
   window.ntc.video = {};
-  window.ntc.video.id = videoId;
+  window.ntc.video.id = window.location.pathname.substr(7);    // Offset is for the '/watch/' part of the URL
+
+  // videoType = window.netflix.falkorCache.videos[window.ntc.video.id].summary.type;
   // window.ntc.video.title = window.netflix.falkorCache.videos[videoId].title.value;
 }
